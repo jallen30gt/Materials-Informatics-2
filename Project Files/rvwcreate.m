79f9d9c4a975d125969bada84798ce9f96068469
Jason Allen <jallen30@gatech.edu>
2016-04-07 20:02:36 -0400

    Project Files

---
+++ Project Files/rvwcreate.m
@@ -0,0 +1,83 @@
+
+filename = input('What file to open? ', 's'); 
+
+num = input('Number of Voxels?', 's');
+num = str2double(num);
+
+width = input('Voxel width?', 's');
+n1 = str2double(width);
+
+height = input('Voxel height?', 's');
+n2 = str2double(height);
+
+depth = input('Voxel depth?', 's');
+n3 = str2double(depth);
+
+binaryimage = matfile(filename);
+
+[x,y,z] = size(binaryimage);
+
+if (x <= n1 || y <= n2 || z <= n3)
+    
+    fprintf('Cannot Compute\n');
+    return;
+    
+else
+    
+end
+    
+
+numvoxwidth = floor(x/n1);
+numvoxheight = floor(y/n2);
+numvoxdepth = floor(z/n3);
+
+numvox = numvoxwidth*numvoxheight*numvoxdepth;
+
+randxarray(1, numvox) = 0;
+randyarray(1, numvox) = 0;
+randzarray(1, numvox) = 0;
+
+voxel(numvoxwidth, numvoxheight, numvoxdepth) = 0;
+
+for i=1:num;    
+         
+     randxarray(1,i) = ceil(rand(1)*numvoxwidth);
+     randyarray(1,i) = ceil(rand(1)*numvoxheight);
+     randzarray(1,i) = ceil(rand(1)*numvoxdepth);
+     
+     while(max(randxarray(1,i) == randxarray) == 1 || max(randyarray(1,i) == randyarray) == 1 || max(randzarray(1,i) == randzarray) == 1)
+         
+        randxarray(1,i) = ceil(rand(1)*numvoxwidth);
+        randyarray(1,i) = ceil(rand(1)*numvoxheight);
+        randzarray(1,i) = ceil(rand(1)*numvoxdepth);
+         
+     end        
+  
+     for j=1:numvoxwidth;
+         
+         for k=1:numvoxheight;
+             
+             for l=1:numvoxdepth;
+                 
+                 voxel(j,k,l) = binaryimage(j,k,l);
+                 
+             end
+             
+         end
+          
+     end
+     
+     newdirectory = input('New directory name?', 's');
+     mkdir(newdirectory);
+     
+     datafile = strcat('C:\Users\jallen30\Desktop\Grad Classes\CSE 8803 - Materials Informatics 2\Project\', newdirectory);
+     
+     cd(datafile);
+     
+     newfilename = strcat('voxel', num2str(i));
+     
+     save(newfilename, voxel);
+     
+end
+
+
