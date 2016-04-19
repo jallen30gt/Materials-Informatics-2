d3c3e8257ed682ae3f28e624c1a7d23b336a83aa
Alicia White <awhite40@gatech.edu>
2016-04-19 18:41:04 -0400

    stacking cubes

--- auto.m
+++ auto.m
@@ -1,16 +1,18 @@
 clear
 clc
-for i = 1:10
+stack_N50 = zeros(10,50,50,50);
+for i = 2:10
     
-    string1 = sprintf('S100_split/voxel%d.mat',i);
+    string1 = sprintf('N_split/voxel%d.mat',i);
     cont = Alicia3D(string1);
     close all
     save(string1, 'cont', '-append');
     %string2 = sprintf('S_%d_cont', i)
     %file = load(string1)
 
-    level = graythresh(cont)
+    level = graythresh(cont);
     seg = cont>level;
     save(string1, 'seg', '-append');
+    stack_N50(i,:,:,:) = seg;
     
 end
